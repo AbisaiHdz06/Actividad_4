3eef083cd2868e10fb9e9a0b0924e65ecd726ff9
AbisaiHdz06 <Al03095697@tecmilenio.mx>
2025-05-06 19:39:35 -0600

    Update Act_4.tf

--- Act_4.tf
+++ Act_4.tf
@@ -125,7 +125,7 @@ resource "aws_instance" "jump_server" {
 
 # --- Web Servers  ---
 resource "aws_instance" "web_server" {
-  count                  = 1
+  count                  = 4
   ami                    = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet_public.id
@@ -136,16 +136,29 @@ resource "aws_instance" "web_server" {
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
+              amazon-linux-extras enable python3.8
+              yum install -y python3.8 mysql
+              pip3 install flask flask-mysqldb
+
+              cat <<EOPYTHON > /home/ec2-user/app.py
+              from flask import Flask
+              app = Flask(__name__)
+
+              @app.route("/")
+              def index():
+                  return "Hola desde Flask en AWS EC2!"
+
+              if __name__ == "__main__":
+                  app.run(host="0.0.0.0", port=80)
+              EOPYTHON
+
+              python3.8 /home/ec2-user/app.py &
               EOF
 
   tags = {
     Name = "WebServer_act3-${count.index + 1}"
   }
-}
-
+} 
 # --- Outputs ---
 output "jump_server_ip" {
   value       = aws_instance.jump_server.public_ip
